f331179768ae3bad010d1e35ed3f35b7d6b9f012
RicardoMolinaCoronel <ridumoli@espol.edu.ec>
2024-07-19 20:03:02 -0500

    Pantalla finalizado y pantallas de instrucciones

---
+++ Scripts/instrucciones_palabra.gd
@@ -0,0 +1,26 @@
+extends Control
+@onready var video_stream_player = $TextureRect/Panel/VideoStreamPlayer
+
+
+func resume():
+	get_tree().paused = false 
+	$AnimationPlayer.play_backwards("blur")
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+func _ready():
+	$AnimationPlayer.play("RESET")
+	pause()
+	video_stream_player.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+
+
+func _on_button_pressed():
+	ButtonClick.button_click()
+	video_stream_player.stop()
+	resume()
