531aacf84813ba323507e498fb09de527f483329
RicardoMolinaCoronel <ridumoli@espol.edu.ec>
2024-09-12 18:52:21 -0500

    SonidosPuntaje

--- Scripts/Puntaje.gd
+++ Scripts/Puntaje.gd
@@ -22,21 +22,29 @@ func updateScore():
 	bonus+=Score.fastBonus+Score.perfectBonus
 	await get_tree().create_timer(1).timeout
 	var tempScore = score
+	$AudioStreamPlayer2D.play(0.0)
 	while(score<(tempScore+Score.newScore)):
 		await get_tree().create_timer(0.01).timeout
 		score+=5
-	await get_tree().create_timer(0.5).timeout
+	$AudioStreamPlayer2D.stop()
+	await get_tree().create_timer(0.8).timeout
+	$AudioStreamPlayer2D.play(0.0)
 	while(fastBonus<Score.fastBonus):
 		await get_tree().create_timer(0.01).timeout
 		fastBonus+=5
-	await get_tree().create_timer(0.5).timeout
+	$AudioStreamPlayer2D.stop()
+	await get_tree().create_timer(0.8).timeout
+	$AudioStreamPlayer2D.play(0.0)
 	while(perfectBonus<Score.perfectBonus):
 		await get_tree().create_timer(0.01).timeout
 		perfectBonus+=5
+	$AudioStreamPlayer2D.stop()
 	await get_tree().create_timer(0.5).timeout
+	$AudioStreamPlayer2D.play(0.0)
 	while(score<(tempScore+Score.newScore+bonus)):
 		await get_tree().create_timer(0.01).timeout
 		score+=5
+	$AudioStreamPlayer2D.stop()
 	
 	#Score.OrderItScore += score
 	#Score.PlayerScore+=score
